24b63666e0b4242e2a52939e7876b88962c82be0
Victor Sollerhed <victor.sollerhed@pagero.com>
2025-03-18 16:40:42 +0100

    Verify `Pod` and `Service` resources

--- approvals/kir_test.go
+++ approvals/kir_test.go
@@ -8,35 +8,39 @@ import (
 	"github.com/mpv/kir/processor"
 )
 
-func TestKir(t *testing.T) {
+func TestKind(t *testing.T) {
 
 	cases := []struct {
 		name string
 		file string
 	}{
 		{
+			name: "Pod",
+			file: "kir_test.TestKind.Pod.input.yaml",
+		},
+		{
 			name: "CronJob",
-			file: "kir_test.TestKir.CronJob.input.yaml",
+			file: "kir_test.TestKind.CronJob.input.yaml",
 		},
 		{
 			name: "DaemonSet",
-			file: "kir_test.TestKir.DaemonSet.input.yaml",
+			file: "kir_test.TestKind.DaemonSet.input.yaml",
 		},
 		{
 			name: "Deployment",
-			file: "kir_test.TestKir.Deployment.input.yaml",
+			file: "kir_test.TestKind.Deployment.input.yaml",
 		},
 		{
 			name: "Job",
-			file: "kir_test.TestKir.Job.input.yaml",
+			file: "kir_test.TestKind.Job.input.yaml",
 		},
 		{
 			name: "ReplicaSet",
-			file: "kir_test.TestKir.ReplicaSet.input.yaml",
+			file: "kir_test.TestKind.ReplicaSet.input.yaml",
 		},
 		{
 			name: "StatefulSet",
-			file: "kir_test.TestKir.StatefulSet.input.yaml",
+			file: "kir_test.TestKind.StatefulSet.input.yaml",
 		},
 	}
 
@@ -44,6 +48,30 @@ func TestKir(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			images, err := processor.ProcessFile(tc.file)
 			if err != nil {
+				t.Fatalf("ProcessFile() error = %v", err)
+			}
+			imagesAsString := strings.Join(images, "\n")
+			approvals.VerifyString(t, imagesAsString)
+		})
+	}
+}
+
+func TestError(t *testing.T) {
+
+	cases := []struct {
+		name string
+		file string
+	}{
+		{
+			name: "Service",
+			file: "kir_test.TestError.Service.input.yaml",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			images, err := processor.ProcessFile(tc.file)
+			if err == nil {
 				t.Fatalf("ProcessFile() error = %v", err)
 			}
 			imagesAsString := strings.Join(images, "\n")
